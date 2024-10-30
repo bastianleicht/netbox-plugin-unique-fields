fa7fea33d87eb03a9a8d68f95a77ea3731d6a556
Bastian Leicht <b.leicht@bastianleicht.de>
2024-10-30 15:13:26 +0100

    fix: correct plugin name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='netbox_role_custom_fields',
+    name='netbox-role-custom-fields',
     version='0.1.1',
     description='Plugin for unique "custom" fields in Netbox',
     url='',
