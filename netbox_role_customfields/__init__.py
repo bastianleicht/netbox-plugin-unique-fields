3ee0341c35e5ed7f6d03abe94422c5dbf33a4d30
Bastian Leicht <b.leicht@bastianleicht.de>
2024-10-31 15:01:22 +0100

    Testing

---
+++ netbox_role_customfields/__init__.py
@@ -0,0 +1,16 @@
+from netbox.plugins import PluginConfig
+
+
+class RoleCustomFieldsConfig(PluginConfig):
+    name = 'netbox-role-customfields'
+    verbose_name = 'Role Specific Custom Fields'
+    description = 'Restrict custom fields to specific device roles'
+    version = '0.1.1'
+    author = 'Bastian Leicht'
+    author_email = 'bl@city-pc.de'
+    base_url = 'role-custom-fields'
+    required_settings = []
+    default_settings = {}
+
+config = RoleCustomFieldsConfig
+
--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='netbox-role-custom-fields',
-    version='0.1.1',
-    description='Plugin for unique "custom" fields in Netbox',
-    url='',
-    author='Bastian Leicht <bl@city-pc.de>',
+    name='netbox-role-customfields',
+    version='0.1',
+    description='A NetBox plugin to manage custom fields for device roles',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        # Hier kannst du Abhängigkeiten eintragen, falls das Plugin welche benötigt
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
